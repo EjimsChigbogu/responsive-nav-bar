baf5a82da4419ca5ddd47b69fa40237105455c7e
Ejims Chigbogu <stanleychigbogu@gmail.com>
2021-07-18 21:22:55 -0700

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+$('.nav-btn').click( () => {
+    $('.nav-bar .nav-links').toggleClass('.active')
+    $('.nav-btn i').toggleClass('.active')
+   
+})
+})
+
+// const navBtn = document.querySelector('.nav-btn')
+// const navBar = document.querySelector('.nav-bar')
+// const navLinks = document.querySelector('.nav-links')
+
+// navBtn.addEventListener('click', () => {
+//     navBar.classList.add('active')
+   
+// })
